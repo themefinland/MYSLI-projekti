72114e2fb0b4765ed59d248a140ca6fa317e970a
deduskaa <jenny.tran@metropolia.fi>
2014-12-08 15:14:27 +0200

    Update kyselyt.sql

--- kyselyt.sql
+++ kyselyt.sql
@@ -1,34 +1,36 @@
-- Käyttäjänä haluan lisätä uuden sienilajikkeen
+Käyttäjänä haluan lisätä uuden sienilajikkeen
 INSERT INTO
 sienilajike(ID, lat_nimi, kut_nimi, loytaja, paikka, uhanalaisuus, ulkonako)
 VALUES(1,"Ephedra alata","Decne.",1,NULL,"Least Concern",1);
 
-- Käyttäjänä haluan etsiä lajiketta sen kutsumanimellä
+Käyttäjänä haluan etsiä lajiketta sen kutsumanimellä
 select * from sienilajike where kut_nimi="Decne.";
 
-- Käyttäjänä haluan etsiä lajiketta sen latinankielisellä nimellä
+Käyttäjänä haluan etsiä lajiketta sen latinankielisellä nimellä
 select * from sienilajike where lat_nimi="Ephedra altissima";
 
-- Käyttäjänä haluan tietää kuka lajikkeen on löytänyt
+Käyttäjänä haluan tietää kuka lajikkeen on löytänyt
 select * from sienilajike, loytaja where kut_nimi="Decne.";
 
-- Käyttäjänä haluan tietää onko lajike uhanalainen
+Käyttäjänä haluan tietää mitkä lajikeet ovat uhanalaisia
 select * from sienilajike where uhanalaisuus="Endangered" 
 
-- Käyttäjänä haluan tietää minkä värinen kyseinen sienilajike on
+Käyttäjänä haluan tietää mitkä lajikkeet ovat tietyn värisiä
+select * from sienilajike, ulkonako where ulkonako.vari="vihreä" 
 
-- Käyttäjänä haluan etsiä lajiketta löytäjän nimellä
+Käyttäjänä haluan etsiä lajikeita löytäjän nimellä
+select * from sienilajike, loytaja where loytaja.nimi="Florin"
 
-- Käyttäjänä haluan etsiä lajikkeita ulkonäön perusteella
+Käyttäjänä haluan etsiä lajikkeita ulkonäön perusteella
 select * from sienilajike where ulkonako=5;
 
-- Käyttäjänä haluan tietää lajikkeen löytäjän kansallisuuden
+Käyttäjänä haluan tietää lajikkeen löytäjän kansallisuuden
 
-- Etsii Latinalaisen nimen ja uhanalaisuuden niiden sienilajikkeiden joiden väri on vihreä
+Etsii Latinalaisen nimen ja uhanalaisuuden niiden sienilajikkeiden joiden väri on vihreä
 select lat_nimi, uhanalaisuus from sienilajike where ulkonako=1;
 
-- Etsii taulukosta latinalaiset nimet jotka sisältää osan "ca"
+Etsii taulukosta latinalaiset nimet jotka sisältää osan "ca"
 select * from sienilajike where lat_nimi like '%ca%';
 
-- Sienet jotka eivät sijaitse euraasiassa.
+Sienet jotka eivät sijaitse euraasiassa.
 select * from sienilajike, paikka where not manner="Euraasia" 
